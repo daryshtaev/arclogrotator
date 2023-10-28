54bb186bb484b461146c5a295b680bc3e483b402
Арыштаев Денис <giperbola@bk.ru>
2023-10-29 01:11:25 +0800

    Добавлена защита от случаев, когда в переменные из приклада могут передать None.

--- arclogrotator.py
+++ arclogrotator.py
@@ -1,5 +1,5 @@
 # https://github.com/daryshtaev/arclogrotator
-# version 1.0.3
+# version 1.0.4
 
 import gzip
 import logging
@@ -19,12 +19,18 @@ def rotator(source, dest):
     os.remove(source)
 
 
-def init_logger(file='app.log', directory=os.path.dirname(os.path.realpath(__file__)), max_bytes=524288, backup_count=10, log_level=logging.WARNING):
+def init_logger(directory=os.path.dirname(os.path.realpath(__file__)), file='app.log', max_bytes=524288, backup_count=10, level=logging.INFO):
+    # Защита на случай, если из приклада передали "file = None", такое случается, если параметр читают из properties-файла:
+    if file is None:
+        file = 'app.log'
+    # Защита на случай, если из приклада передали "level = None", такое случается, если параметр читают из properties-файла::
+    if level is None:
+        level = logging.INFO
     log_file = os.path.join(directory, file)
     log_format = '%(asctime)s.%(msecs)03d,%(threadName)s,%(levelname)s,%(name)s,%(message)s'
     log_date_format = '%d.%m.%y %H:%M:%S'
     log_formatter = logging.Formatter(log_format, datefmt=log_date_format)
-    # Пример инициализации лога, который "вращается" по моменту времени - в частности "каждую полночь":
+    # Пример инициализации лога, который "вращается" по моменту времени - в частности "каждую полночь (midnight)":
     #logFileHandler = TimedRotatingFileHandler(logFile, when='midnight', interval=1, backupCount=10)
     # Инициализация лога, который "вращается" по размеру: "каждые maxBytes":
     log_file_handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count, encoding='utf-8')
@@ -33,5 +39,5 @@ def init_logger(file='app.log', directory=os.path.dirname(os.path.realpath(__fil
     log_file_handler.namer = namer
     logger = logging.getLogger()
     logger.addHandler(log_file_handler)
-    logger.setLevel(log_level)
+    logger.setLevel(level)
     return logger
